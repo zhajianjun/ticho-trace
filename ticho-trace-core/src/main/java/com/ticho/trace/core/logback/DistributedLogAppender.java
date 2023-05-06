8a3c8987cb0a9786e661aecfc268fd6768b9f4bc
zhajianjun <1019319473@qq.com>
2023-05-06 11:28:59 +0800

    异常堆栈信息补充

--- ticho-trace-core/src/main/java/com/ticho/trace/core/logback/DistributedLogAppender.java
+++ ticho-trace-core/src/main/java/com/ticho/trace/core/logback/DistributedLogAppender.java
@@ -1,11 +1,16 @@
 package com.ticho.trace.core.logback;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.ThrowableProxy;
 import ch.qos.logback.core.AppenderBase;
-import lombok.Setter;
 import com.ticho.trace.common.constant.LogConst;
 import com.ticho.trace.core.handle.LogHandleContext;
+import lombok.Setter;
+import org.slf4j.helpers.MessageFormatter;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -74,7 +79,7 @@ public class DistributedLogAppender extends AppenderBase<ILoggingEvent> {
             logInfo.setLogLevel(event.getLevel().toString());
             logInfo.setDtTime(event.getTimeStamp());
             logInfo.setClassName(event.getLoggerName());
-            logInfo.setContent(event.getFormattedMessage());
+            logInfo.setContent(getMessage(event));
             logInfo.setThreadName(event.getThreadName());
             logInfo.setMdc(mdcMap);
             String method = "";
@@ -87,6 +92,55 @@ public class DistributedLogAppender extends AppenderBase<ILoggingEvent> {
             }
             logInfo.setMethod(method);
         });
+    }
+
+    private static String getMessage(ILoggingEvent logEvent) {
+        if (!logEvent.getLevel().equals(Level.ERROR)) {
+            return logEvent.getFormattedMessage();
+        }
+        if (logEvent.getThrowableProxy() != null) {
+            ThrowableProxy throwableProxy = (ThrowableProxy) logEvent.getThrowableProxy();
+            String arg = logEvent.getFormattedMessage() + "\n" + errorStackTrace(throwableProxy.getThrowable()).toString();
+            return packageMessage("{}", arg);
+        }
+        Object[] args = logEvent.getArgumentArray();
+        if (args != null) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i] instanceof Throwable) {
+                    args[i] = errorStackTrace(args[i]);
+                }
+            }
+            return packageMessage(logEvent.getMessage(), args);
+        }
+        return logEvent.getFormattedMessage();
+    }
+
+    private static String packageMessage(String message, Object... args) {
+        if (message != null && message.contains("{}")) {
+            return MessageFormatter.arrayFormat(message, args).getMessage();
+        }
+        StringBuilder builder = new StringBuilder(128);
+        builder.append(message);
+        for (Object arg : args) {
+            builder.append("\n").append(arg);
+        }
+        return builder.toString();
+    }
+
+    public static Object errorStackTrace(Object obj) {
+        if (obj instanceof Exception) {
+            Exception eObj = (Exception) obj;
+            try (StringWriter sw = new StringWriter(); PrintWriter pw = new PrintWriter(sw)) {
+                String exceptionStack;
+                eObj.printStackTrace(pw);
+                exceptionStack = sw.toString();
+                return exceptionStack;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return obj;
+            }
+        }
+        return obj;
     }
 
 }
