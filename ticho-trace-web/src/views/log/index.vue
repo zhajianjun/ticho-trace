a1307ecc6f5ca16cdbc30c9fb0dd65aa361c7e43
zhajianjun <1019319473@qq.com>
2023-05-13 07:49:06 +0800

    web项目单独迁出

--- ticho-trace-web/src/views/log/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>log</h1>
-</template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style lang="less" scoped></style>
--- ticho-trace-web/src/views/system/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>system</h1>
-</template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style lang="less" scoped></style>
